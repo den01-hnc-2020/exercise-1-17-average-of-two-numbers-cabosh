e2f578cb173f40b033b7798a621e7ba0a365f138
cabosh <callum.bosher@gmail.com>
2020-05-15 15:23:57 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    total = num1 + num2
+
+    print("The average is " + str(total / 2))
 
 if __name__ == '__main__':
     main()
